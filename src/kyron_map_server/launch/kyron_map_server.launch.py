cf2106b4bb3c22d3bd61b742e56cafa07640a440
Ariel <ariel507182003@gmail.com>
2025-03-25 10:56:22 +0100

    EscaneoMapa_11 comentarios anyadidos
    Solo comentarios en el launch

--- src/kyron_map_server/launch/kyron_map_server.launch.py
+++ src/kyron_map_server/launch/kyron_map_server.launch.py
@@ -16,6 +16,8 @@ def generate_launch_description():
 
     return LaunchDescription([
         Node(
+            #Contiene un servidor que se encargará de proporcionarnos el mapa. Este servidor queda abierto, 
+            # a la espera de recibir una petición de carga del mapa. Para ello utilzará la llamada al servicio map_server/load_map
             package='nav2_map_server',
             executable='map_server',
             name='map_server',
@@ -25,6 +27,7 @@ def generate_launch_description():
                        ]),
 
         Node(
+            #lanza el visualizador RVIZ con la configuración que hemos guardado en el directorio rviz
             package='rviz2',
             executable='rviz2',
             name='rviz2',
@@ -34,6 +37,10 @@ def generate_launch_description():
             ),
 
         Node(
+            #se trata de un nodo que se encarga de controlar el ciclo de vida de los nodos ROS2. 
+            # Lanza una máquina de estados que decidirá en qué estado se encuentra cada nodo (unconfigured, inactive, active o finalized).
+            # Para saber más acerca de este tema podéis acceder a la siguiente web. 
+            # Este manejador del ciclo de vida de los nodos permite que el sistema tenga un comportamiento determinístico y facilita la depuración del mismo.
             package='nav2_lifecycle_manager',
             executable='lifecycle_manager',
             name='lifecycle_manager_localization',
