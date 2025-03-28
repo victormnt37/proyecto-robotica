7d7bfa5d93d400706a4feddcedb2a6f1c5f357f8
Ariel <ariel507182003@gmail.com>
2025-03-28 12:20:05 +0100

    Dezplazamiento_12
    Ccheckpoint

---
+++ src/kyron/kyron_nav/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'kyron_nav'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.rviz')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.pgm'))
+
+    ],
+    
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='admin',
+    maintainer_email='ariel507182003@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'initial_pose_pub = kyron_nav.initial_pose_pub:main'
+        ],
+    },
+)
