fcedc63f78f8375195259bdee9d9940f1e813390
victormnt37 <victormorantf@gmail.com>
2025-06-03 18:22:22 +0200

    T010 - el paciente manda al robot a la sala

--- web-page/js/citas.js
+++ web-page/js/citas.js
@@ -1,3 +1,16 @@
+let wsControl = new WebSocket("ws://localhost:8765");
+
+function sendGoal(sala) {
+    const msg = {
+        command: "go_to_goal",
+        x: sala.x,
+        y: sala.y,
+        w: sala.w
+    };
+    wsControl.send(JSON.stringify(msg));
+}
+
+// Esta es un cita de ejemplo, en un caso real los datos de la cita se recogerían del backend
 const cita = {
     fecha: "04/06/2025",
     id_paciente: "123456A",
@@ -14,8 +27,6 @@ const cita = {
     motivo: "Resonancia"
 }
 
-console.log('citas');
-
 elementoCita = crearCita(cita);
 
 document.getElementById('id_paciente').addEventListener('keyup', (e) => {
@@ -27,7 +38,7 @@ document.getElementById('id_paciente').addEventListener('keyup', (e) => {
 })
 
 function crearCita(cita) {
-    tabla = document.getElementById('citas-table');
+    tabla = document.getElementById('citas-table').lastElementChild;
 
     const fila = document.createElement('tr');
     fila.style = 'display: none';
@@ -40,6 +51,32 @@ function crearCita(cita) {
 
     fila.appendChild(celdaFecha);
     fila.appendChild(celdaSala);
+    fila.addEventListener('click', () => {
+        document.getElementById('popup-fecha').textContent = cita.fecha;
+        document.getElementById('popup-hora').textContent = cita.hora;
+        document.getElementById('popup-paciente').textContent = cita.nombre_paciente;
+        document.getElementById('popup-doctor').textContent = cita.nombre_doctor;
+        document.getElementById('popup-motivo').textContent = cita.motivo;
+        document.getElementById('popup-sala').textContent = cita.sala.nombre;
+
+        document.getElementById('popup').classList.remove('hidden');
+    });
+
+    // cancelar pop-up
+    document.getElementById('cerrar-popup').addEventListener('click', () => {
+        document.getElementById('popup').classList.add('hidden');
+    });
+    document.getElementById('cancelar-popup').addEventListener('click', () => {
+        document.getElementById('popup').classList.add('hidden');
+    });
+
+    // dirigirse a sala
+    document.getElementById('dirigir-a-sala').addEventListener('click', () => {
+        sendGoal(cita.sala);
+        document.getElementById('popup').classList.add('hidden');
+
+        mostrarToast(`Sigue al robot Kyron a la sala: ${cita.sala.nombre}`);
+    })
 
     tabla.appendChild(fila);
 
@@ -54,4 +91,15 @@ function ocultarCita(elementoCita) {
     elementoCita.style = 'display: none';
 }
 
-// TODO: que cuando clique aparezca el popup de datos de la cita y la sala
+function mostrarToast(mensaje, duracion = 3000) {
+    const toast = document.getElementById('toast');
+    toast.textContent = mensaje;
+    toast.classList.add('show');
+    toast.classList.remove('hidden');
+  
+    setTimeout(() => {
+      toast.classList.remove('show');
+      toast.classList.add('hidden');
+    }, duracion);
+  }
+  
